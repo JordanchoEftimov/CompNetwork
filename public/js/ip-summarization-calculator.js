bb191e0a4e716c3e7cebcee810f77984135372bd
JordanchoEftimov <ejordancho@gmail.com>
2021-02-12 02:21:14 +0100

    Added icons to buttons

--- public/js/ip-summarization-calculator.js
+++ public/js/ip-summarization-calculator.js
@@ -105,3 +105,15 @@ function errorMessage(message) {
     }
     showMessage();
 }
+
+function copyToClipboardFunction() {
+    /* Get the text field */
+    let copyText = document.getElementById("summar-ip-addr");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  }
